df51b49f762e1b31426afa99684ea3f467a0e554
dev-lester94 <dev.lester94@gmail.com>
2022-07-03 16:18:39 -0500

    push project

---
+++ Shared/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  Shared
+//
+//  Created by lester on 7/2/22.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
